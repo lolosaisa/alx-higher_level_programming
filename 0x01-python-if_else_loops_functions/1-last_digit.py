ee79317589d2c6c0c700b3864ddfa4be9d34db85
lolosaisa <lwamaitha3924@gmail.com>
2023-12-08 07:41:47 -0800

    Handling positive

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,9 +5,12 @@ dig = abs(number) % 10
 if number < 0:
     dig = -dig
     print("Last digit of {} is {} and is ".format(number, dig), end = "")
+elif number > 0:
+    dig = +dig
+    print("Last digit of {} is {} and is ".format(number, dig), end = "")
     if dig > 5:
-        print("and is greater than 5")
+        print("greater than 5")
     elif dig == 0:
-        print("and is 0")
+        print("0")
     else:
         print("and is less than 6 and not 0")
