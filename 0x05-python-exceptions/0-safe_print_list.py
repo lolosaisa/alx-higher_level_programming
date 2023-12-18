7152bee0388e18c83bdff770191358fa2f26f099
lolosaisa <lwamaitha3924@gmail.com>
2023-12-18 12:33:21 -0800

    fixed the Type error

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    indexcount = 0
+    num = 0
     try:
-        while indexcount is not x:
-            print(my_list[indexcount], end="")
-            indexcount +=1
+        while num is not x:
+            print(my_list[num], end="")
+            num += 1
     except IndexError:
-        pass
-        print()
-        return indexcount
+        None
+    print()
+    return num
