47ee8b13fec718220644c2e2d4f104d9d5576179
lolosaisa <lwamaitha3924@gmail.com>
2024-05-03 15:55:42 +0300

    js cript that updates the text color of the <header> element to red (#FF0000):

--- 0x15-javascript-web_jquery/1-script.js
+++ 0x15-javascript-web_jquery/1-script.js
@@ -1,5 +1,2 @@
-#!/usr/bin/node
-const fs = require('fs');
-fs.writeFile(process.argv[2], process.argv[3], error => {
-	  if (error) console.log(error);
-});
+const $ = window.$;
+$('HEADER').css('color', '#FF0000');
